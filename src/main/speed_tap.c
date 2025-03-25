c0f794ae52373de4534c943336cb400f1bb3a864
biirdrat <steventubusiness@gmail.com>
2025-03-25 00:00:31 -0500

    feat: add process button task with semaphore

--- src/main/speed_tap.c
+++ src/main/speed_tap.c
@@ -3,34 +3,55 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "driver/i2c.h"
 #include "driver/gptimer.h"
 #include "i2c_lcd.h"
 
 #define LCD_MESSAGE_BUFFER_SIZE 17
+#define NUM_BUTTONS 4
 
-uint8_t const LED_PIN = 2;
-uint8_t LED0_PIN = 4;
-uint8_t LED1_PIN = 16;
-uint8_t LED2_PIN = 17;
-uint8_t LED3_PIN = 5;
-uint8_t BUTTON0_PIN = 27;
-uint8_t BUTTON1_PIN = 26;
-uint8_t BUTTON2_PIN = 5;
-uint8_t BUTTON3_PIN = 33;
+typedef enum 
+{
+    BUTTON_RELEASED = 0,
+    BUTTON_PRESSED = 1
+} button_state;
+
+typedef struct 
+{
+    uint8_t button_pin;
+    uint8_t current_input;
+    uint8_t previous_input;
+    button_state state;
+} button;
 
 static const char *TAG = "MAIN";
+
+const uint8_t LED_PIN = 2;
+const uint8_t LED0_PIN = 4;
+const uint8_t LED1_PIN = 16;
+const uint8_t LED2_PIN = 17;
+const uint8_t LED3_PIN = 5;
+const uint8_t BUTTON0_PIN = 27;
+const uint8_t BUTTON1_PIN = 26;
+const uint8_t BUTTON2_PIN = 5;
+const uint8_t BUTTON3_PIN = 33;
+
+SemaphoreHandle_t process_buttons_semaphore;
 
 char lcd_message_buffer[LCD_MESSAGE_BUFFER_SIZE];
 
 gptimer_handle_t program_timer = NULL;
-gptimer_handle_t detect_button_timer = NULL;
+gptimer_handle_t process_buttons_timer = NULL;
 uint64_t start_count = 0;
 uint64_t end_count = 0;
 
+button buttons_array[NUM_BUTTONS];
+
+void process_buttons_task(void *pvParameter);
 void initialize_gpio_pins();
+void initialize_buttons();
 void initialize_lcd();
 void initialize_timers();
+void initialize_semaphores();
 
 void app_main(void)
 {
@@ -39,42 +60,44 @@ void app_main(void)
 
     initialize_gpio_pins();
 
+    initialize_buttons();
+
     initialize_lcd();
 
     initialize_timers();
 
+    initialize_semaphores();
+
     ESP_LOGI(TAG, "Main Program Finished!\n");
-    // Create the FreeRTOS task to blink LED
-    // xTaskCreate(&led_blink_task, "LED Blink Task", 2048, NULL, 5, NULL);
+
+    xTaskCreate(&process_buttons_task, "Process Buttons Task", 2048, NULL, 5, NULL);
 }
 
-void led_blink_task(void *pvParameter)
-{
-    // Configure the LED GPIO as an output
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
-
-    while (1)
-    {
-        gpio_set_level(LED_PIN, 1); // Turn LED ON
-        printf("LED ON\n");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
-
-        gpio_set_level(LED_PIN, 0); // Turn LED OFF
-        printf("LED OFF\n");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay for 1 second
-    }
-}
-
-static bool IRAM_ATTR detect_button_timer_on_alarm(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+static bool IRAM_ATTR process_buttons_timer_on_alarm(
+    gptimer_handle_t timer,
+    const gptimer_alarm_event_data_t *edata,
+    void *user_data
+    )
 {
     BaseType_t high_task_awoken = pdFALSE;
-    // ESP_EARLY_LOGI(TAG, "Timer Alarm Triggered! Count: %llu", edata->count_value);
 
-    gptimer_get_raw_count(program_timer, &end_count);
-    ESP_EARLY_LOGI(TAG, "Elapsed Count: %llu", end_count - start_count);
-    start_count = end_count;
+    // Give the semaphore from ISR
+    xSemaphoreGiveFromISR(process_buttons_semaphore, &high_task_awoken);
+
     // return whether we need to yield at the end of ISR
     return (high_task_awoken == pdTRUE);
+}
+
+void process_buttons_task(void *pvParameter)
+{
+    while (1)
+    {
+        // Wait indefinitely for the semaphore
+        if (xSemaphoreTake(process_buttons_semaphore, portMAX_DELAY) == pdTRUE)
+        {
+            ESP_LOGI(TAG, "Processing Buttons from task\n");
+        }
+    }
 }
 
 void initialize_gpio_pins()
@@ -108,6 +131,21 @@ void initialize_gpio_pins()
     gpio_config(&io_config_output);
 }
 
+void initialize_buttons()
+{
+    buttons_array[0].button_pin = BUTTON0_PIN;
+    buttons_array[1].button_pin = BUTTON1_PIN;
+    buttons_array[2].button_pin = BUTTON2_PIN;
+    buttons_array[3].button_pin = BUTTON3_PIN;
+
+    for(int button_idx = 0; button_idx < NUM_BUTTONS; button_idx++)
+    {
+        buttons_array[button_idx].current_input = 0;
+        buttons_array[button_idx].previous_input = 0;
+        buttons_array[button_idx].state = BUTTON_RELEASED;
+    }
+}
+
 void initialize_lcd()
 {
         // Initialize the I2C bus
@@ -139,7 +177,7 @@ void initialize_timers()
     };
 
     // Set the configuration for the detect button timer
-    gptimer_config_t detect_button_timer_config = 
+    gptimer_config_t process_buttons_timer_config = 
     {
         .clk_src = GPTIMER_CLK_SRC_DEFAULT,
         .direction = GPTIMER_COUNT_UP,
@@ -147,13 +185,13 @@ void initialize_timers()
     };
 
     // Set the callback function for the detect button timer
-    gptimer_event_callbacks_t detect_button_callbacks = 
+    gptimer_event_callbacks_t process_buttons_callbacks = 
     {
-        .on_alarm = detect_button_timer_on_alarm,
+        .on_alarm = process_buttons_timer_on_alarm,
     };
 
     // Set the configuration for the detect button timer
-    gptimer_alarm_config_t detect_button_alarm_config = 
+    gptimer_alarm_config_t process_buttons_alarm_config = 
     {
         .alarm_count = 5000000,
         .reload_count = 0,
@@ -166,12 +204,18 @@ void initialize_timers()
     ESP_ERROR_CHECK(gptimer_start(program_timer));
 
     // Create the detect button timer, register callbacks, and enable timer
-    ESP_ERROR_CHECK(gptimer_new_timer(&detect_button_timer_config, &detect_button_timer));
-    ESP_ERROR_CHECK(gptimer_register_event_callbacks(detect_button_timer, &detect_button_callbacks, NULL));
-    ESP_ERROR_CHECK(gptimer_set_alarm_action(detect_button_timer, &detect_button_alarm_config));
-    ESP_ERROR_CHECK(gptimer_enable(detect_button_timer));
-    ESP_ERROR_CHECK(gptimer_start(detect_button_timer));
-
-    gptimer_get_raw_count(program_timer, &start_count);
+    ESP_ERROR_CHECK(gptimer_new_timer(&process_buttons_timer_config, &process_buttons_timer));
+    ESP_ERROR_CHECK(gptimer_register_event_callbacks(process_buttons_timer, &process_buttons_callbacks, NULL));
+    ESP_ERROR_CHECK(gptimer_set_alarm_action(process_buttons_timer, &process_buttons_alarm_config));
+    ESP_ERROR_CHECK(gptimer_enable(process_buttons_timer));
+    ESP_ERROR_CHECK(gptimer_start(process_buttons_timer));
 }
 
+void initialize_semaphores()
+{
+    process_buttons_semaphore = xSemaphoreCreateBinary();
+    if(process_buttons_semaphore == NULL)
+    {
+        ESP_LOGE(TAG, "Failed to create process_buttons_semaphore\n");
+    }
+}
